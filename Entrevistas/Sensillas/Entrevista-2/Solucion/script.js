2181b17dbd00fb943e792108bc3fc5a27211c4d1
damiansire <damiansire@hotmail.com>
2020-12-08 22:44:34 -0300

    Add burblesort

--- Entrevistas/Sensillas/Entrevista-2/Solucion/script.js
+++
@@ -1,9 +0,0 @@
-// numbers = [6, 9, 13, 3, 1]
-//.sort(function(a, b){return a-b});
-
-function removeDuplicates(array) {
-    let mySet = new Set(array)
-    return Array.from(mySet)
-}
-
-myList = list(set(myList))
